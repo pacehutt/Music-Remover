b9828d541286c23294d9c9b103c77d802030a6a9
Mafiz <mafizonly@gmail.com>
2023-12-27 23:12:25 +0530

    multiple routes added for vocals splitter api

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const express = require("express");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+
+const uploadService = require("./controllers/upload");
+const { pollingService } = require("./controllers/polling");
+const {
+  downloadMusicService,
+  downloadVocalService,
+} = require("./controllers/download");
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
-const routes = require("./routes/file.routes");
-
-const { upload } = require("./middlewares/upload");
+app.use(cors());
+app.use(fileUpload());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("🛠️Music Splitter up and running😶‍🌫️!");
+  // send html file index.html stored in root directory
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/api/upload", upload.single("file"), routes.handleUpload);
+app.post("/api/upload", uploadService);
+app.get("/api/process", pollingService);
+app.get("api/download/vocal", downloadVocalService);
+app.get("api/download/music", downloadMusicService);
 
 app.listen(port, () =>
-  console.log(`Music Splitter listening on port 
+  console.log(`Vocals Splitter listening on port 
 ${port}!`)
 );
